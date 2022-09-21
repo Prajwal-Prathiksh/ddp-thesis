2f36149134b413d61d0d20c2031f8e0ca7891dd1
K T Prajwal Prathiksh <prajwal.prathiksh@gmail.com>
2022-09-21 14:51:17 +0530

    Add file for delta LES-SPH Scheme

---
+++ code/deltaLES.py
@@ -0,0 +1,11 @@
+r"""
+:math:`\delta` LES-SPH
+#####################
+References
+-----------
+    .. [Colagrossi2021] A. Colagrossi, “Smoothed particle hydrodynamics method
+    from a large eddy simulation perspective . Generalization to a
+    quasi-Lagrangian model Smoothed particle hydrodynamics method from a
+    large eddy simulation perspective . Generalization to a quasi-Lagrangian
+    model,” vol. 015102, no. December 2020, 2021, doi: 10.1063/5.0034568.
+"""
