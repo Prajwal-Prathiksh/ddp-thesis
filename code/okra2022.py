ad88893d724c5bb7ae7f44e094333aab2465d0dc
K T Prajwal Prathiksh <prajwal.prathiksh@gmail.com>
2022-10-13 01:19:23 +0530

    Added all equations

--- code/okra2022.py
+++ code/okra2022.py
@@ -12,7 +12,9 @@ References
 ###########################################################################
 # Particle Array 
 ###########################################################################
+from curses.ascii import DEL
 from signal import raise_signal
+from xmlrpc.client import TRANSPORT_ERROR
 from pysph.base.utils import get_particle_array
 
 ### `\delta^+` Fluid Particle Array
@@ -27,7 +29,7 @@ def get_particle_array_sph_les_fluid(constants=None, **props):
                 'aw', 'gid', 'pid', 'tag',
                 
                 # SPH-LES Properties
-                'ax', 'ay', 'az', 'V', 'tau_sfs', 'J', 'nu_t'
+                'ax', 'ay', 'az', 'V', 'tau', 'J', 'nu_t'
             ]
 
         Parameters:
@@ -53,7 +55,7 @@ def get_particle_array_sph_les_fluid(constants=None, **props):
         constants=constants, additional_props=sph_les_props, **props
     )
 
-    pa.add_property('tau_sfs', stride=9)
+    pa.add_property('tau', stride=9)
     pa.add_property('J', stride=9)
 
     # default property arrays to save out
@@ -136,12 +138,137 @@ class SummationDensity(Equation):
 
 
 ### Momentum Equation------------------------------------------------------
+from compyle.api import declare
+class PreMomentumEquation(Equation):
+    def __init__(
+        self, dest, sources, dim, nu, rho0, turb_visc_model, DELTA, Cs=0.15,
+        C_sigma=1.35
+    ):
+        r"""
+        Parameters
+        ----------
+        dim : int
+            Dimensions of the problem
+        nu : float
+            Kinematic Viscosity
+        rho0 : float
+            Reference Density
+        turb_visc_model : str
+            Turbulent Viscosity Model to be used: SMAG, SIGMA, SMAG_MCG
+        DELTA : float
+            Filter Length-scale
+        Cs : float
+            Smagorinsky Constant
+        Csigma : float
+            Sigma Model Constant
+        """
+
+        self.dim = int(dim)
+        self.nu = nu
+        self.rho0 = rho0
+        self.turb_visc_model = turb_visc_model
+        self.DELTA = DELTA
+        self.Cs = Cs
+        self.C_sigma = C_sigma
+
+        self.lap_vel_prefactor = 2. * (2. + self.dim) * self.nu * self.rho0
+        self.SMAG_prefactor = (self.Cs*self.DELTA)**2
+        self.SIGMA_prefactor = (self.C_sigma*self.DELTA)**2
+        
+        turb_visc_models = ['SMAG', 'SIGMA', 'SMAG_MCG']
+        if self.turb_visc_model not in turb_visc_models:
+            raise Exception("Invalid Turbulent Viscosity Model.")
+
+        super(PreMomentumEquation, self).__init__(dest, sources)
+
+    def initialize(self, d_idx, d_J, d_tau):
+        i = declare('int', 1)
+        for i in range(9):
+            d_J[d_idx*9 + i] = 0.
+            d_tau[d_idx*9 + i] = 0.
+
+    def loop(self, d_idx, s_idx, d_J, s_V, VIJ, DWIJ):
+        
+        Vj = s_V[s_idx]
+        
+        i, j = declare('int', 2)
+
+        for i in range(3):
+            for j in range(3):
+                d_J[d_idx*9 + i*3 + j] = - VIJ[i]*DWIJ[j]*Vj
+
+        # d_J[d_idx*9 + 0] += - VIJ[0]*DWIJ[0]*Vj # 2D
+        # d_J[d_idx*9 + 1] += - VIJ[0]*DWIJ[1]*Vj # 2D
+        # d_J[d_idx*9 + 2] += - VIJ[0]*DWIJ[2]*Vj
+
+        # d_J[d_idx*9 + 3] += - VIJ[1]*DWIJ[0]*Vj # 2D
+        # d_J[d_idx*9 + 4] += - VIJ[1]*DWIJ[1]*Vj # 2D
+        # d_J[d_idx*9 + 5] += - VIJ[1]*DWIJ[2]*Vj
+
+        # d_J[d_idx*9 + 6] += - VIJ[2]*DWIJ[0]*Vj
+        # d_J[d_idx*9 + 7] += - VIJ[2]*DWIJ[1]*Vj
+        # d_J[d_idx*9 + 8] += - VIJ[2]*DWIJ[2]*Vj
+
+
+    def post_loop(self, d_idx, d_J, d_nu_t, s_idx, d_tau, s_tau):
+        i, j = declare('int', 2)
+        n = 3
+        D = declare('matrix((3,3))')
+
+        for i in range(n):
+            for j in range(n):
+                D[i*n + j] = 0.5*(
+                    d_J[d_idx*9 + i*n + j] + d_J[d_idx*9 + i + j*n]
+                )
+        
+        if self.dim == 2:
+            trace_D_square = D[0]**2 + D[4]**2 + 2*D[1]**2
+        elif self.dim == 3:
+            trace_D_square = D[0]**2 + D[4]**2 + D[8]**2
+            trace_D_square += 2*(D[1]**2 + D[2]**2 + D[5]**2)
+
+
+        if self.turb_visc_model == "SMAG":
+            d_nu_t[d_idx] = self.SMAG_prefactor*sqrt(2*trace_D_square)
+
+        elif self.turb_visc_model == "SIGMA":
+            if self.dim == 2:
+                sigma1 = D[0] + D[4]
+                sigma2 = 0.5*(sigma1**2 - trace_D_square)
+                sigma3 = D[0]*D[4] - D[1]**2
+            elif self.dim == 3:
+                sigma1 = D[0] + D[4] + D[8]
+                sigma2 = 0.5*(sigma1**2 - trace_D_square)
+                
+                A = D[0]*(D[4]*D[8] - D[5]*D[7])
+                B = D[1]*(D[3]*D[8] - D[5]*D[6])
+                C = D[2]*(D[3]*D[7] - D[4]*D[6])
+                sigma3 = A - B + C
+
+            d_nu_t[d_idx] = sigma3*(sigma1-sigma2)*(sigma2-sigma3)/sigma1**2
+            d_nu_t[d_idx] *= self.SIGMA_prefactor
+
+        fac = -2.*d_nu_t[d_idx]*self.rho0
+        if self.dim == 2:
+            d_tau[d_idx*9 + 0] = fac*D[0]
+            d_tau[d_idx*9 + 1] = fac*D[1]
+            d_tau[d_idx*9 + 3] = fac*D[3]
+            d_tau[d_idx*9 + 4] = fac*D[4]
+        elif self.dim == 3:
+            for i in range(3):
+                for j in range(3):
+                    d_tau[d_idx*9 + i*n + j] = fac*D[i*n + j]
+
 class MomentumEquation(Equation):
     r"""**Momentum equation**
 
     .. math::
 
-        \bar{\rho}_i \frac{\mathrm{d} \tilde{\mathbf{v}}_i}{\mathrm{~d} t} = -\sum_j \left(\bar{p}_j+\bar{p}_i\right) \nabla W_{ij} V_j+2(2+n) \eta \sum_j \frac{\tilde{\mathbf{v}}_{ij} \cdot \mathbf{r}_{ij}}{|\mathbf{r}_{ij}|^2} \nabla W_{ij} V_j-\operatorname{div}\left[\boldsymbol{\tau}_{S F S}\right]_i
+        \bar{\rho}_i \frac{\mathrm{d} \tilde{\mathbf{v}}_i}{\mathrm{~d} t} =
+        -\sum_j \left(\bar{p}_j+\bar{p}_i\right) \nabla W_{ij} V_j+2(2+n) \eta
+        \sum_j \frac{\tilde{\mathbf{v}}_{ij} \cdot 
+        \mathbf{r}_{ij}}{|\mathbf{r}_{ij}|^2} \nabla W_{ij} V_j-
+        \operatorname{div}\left[\boldsymbol{\tau}_{S F S}\right]_i
 
     where
     
@@ -151,37 +278,51 @@ class MomentumEquation(Equation):
 
     .. math::
 
-        \operatorname{div} \left[\boldsymbol{\tau}_{S F S}\right] (\mathbf{r}, t) \approx \sum_j (\tau_{SFS, j} + \tau_{SFS, i}) \nabla W_{ij} V_j
+        \operatorname{div} \left[\boldsymbol{\tau}_{S F S}\right] 
+        (\mathbf{r}, t) \approx \sum_j (\tau_{SFS, j} + \tau_{SFS, i}) 
+        \nabla W_{ij} V_j
 
     .. math::
 
-        \tau_{SFS}(\mathbf{r}, t) \approx  -2 \nu_t \bar{\rho} \tilde{\mathbb{D}}(\mathbf{r}, t)
+        \tau_{SFS}(\mathbf{r}, t) \approx  -2 \nu_t \bar{\rho} 
+        \tilde{\mathbb{D}}(\mathbf{r}, t)
 
     .. math::
 
-        \tilde{\mathbb{D}}(\mathbf{r}, t) = \frac{1}{2} (\tilde{\mathbb{J}} + \tilde{\mathbb{J}}^T) (\mathbf{r}, t)
+        \tilde{\mathbb{D}}(\mathbf{r}, t) = \frac{1}{2} (\tilde{\mathbb{J}} +
+        \tilde{\mathbb{J}}^T) (\mathbf{r}, t)
 
     .. math::
 
-        \tilde{\mathbb{J}}(\mathbf{r}_i, t) \approx  - \sum_j \tilde{\mathbf{v}}_{ij} \nabla W_{ij}^T V_j
+        \tilde{\mathbb{J}}(\mathbf{r}_i, t) \approx  - 
+        \sum_j \tilde{\mathbf{v}}_{ij} \nabla W_{ij}^T V_j
 
-    The turbulence eddy viscosity term can be modelled through either the Smagorinsky model or the \sigma model, given below respectively:
+    The turbulence eddy viscosity term can be modelled through either the 
+    Smagorinsky model or the \sigma model, given below respectively:
     
     .. math::
         
-        \nu_t = (C_S \Delta)^2 \sqrt{2 \operatorname{tr}[\tilde{\mathbb{D}}^2]}, C_S = 0.15
+        \nu_t = (C_S \Delta)^2 \sqrt{2 
+        \operatorname{tr}[\tilde{\mathbb{D}}^2]}, C_S = 0.15
 
     .. math::
 
-        \nu_t = (C_{\sigma} \Delta)^2 \frac{\sigma_3(\sigma_1 - \sigma_2)(\sigma_2 - \sigma_3)}{\sigma_1^2}, C_{\sigma} = 1.35
+        \nu_t = (C_{\sigma} \Delta)^2 \frac{\sigma_3(\sigma_1 - 
+        \sigma_2)(\sigma_2 - \sigma_3)}{\sigma_1^2}, C_{\sigma} = 1.35
 
-    where \sigma refers to the singular values of the tensor \tilde{\mathbb{J}}.
+    where \sigma refers to the singular values of the tensor
+    \tilde{\mathbb{J}}.
 
-    The Monoghan-Cleary-Gingold (MCG) form of the \operatorname{div} \left[\boldsymbol{\tau}_{S F S}\right] is given as:
+    The Monoghan-Cleary-Gingold (MCG) form of the \operatorname{div}
+    \left[\boldsymbol{\tau}_{S F S}\right] is given as:
 
     .. math::
 
-        \operatorname{div} \left[\boldsymbol{\tau}_{S F S}\right] (\mathbf{r}, t) \approx 2(2+n) \sum_j \bar{\rho}_i \bar{\rho}_j \frac{\nu_{t,i} + \nu_{t,j}}{\bar{\rho}_i + \bar{\rho}_j} \frac{\tilde{\mathbf{v}}_{ij} \cdot \mathbf{r}_{ij}}{|\mathbf{r}_{ij}|^2}\nabla W_{ij} Vj
+        \operatorname{div} \left[\boldsymbol{\tau}_{S F S}\right]
+        (\mathbf{r}, t) \approx 2(2+n) \sum_j \bar{\rho}_i \bar{\rho}_j
+        \frac{\nu_{t,i} + \nu_{t,j}}{\bar{\rho}_i + \bar{\rho}_j}
+        \frac{\tilde{\mathbf{v}}_{ij} \cdot 
+        \mathbf{r}_{ij}}{|\mathbf{r}_{ij}|^2}\nabla W_{ij} Vj
 
     """
 
@@ -199,7 +340,7 @@ class MomentumEquation(Equation):
             Turbulent Viscosity Model to be used: SMAG, SIGMA, SMAG_MCG
         """
 
-        self.dim = dim
+        self.dim = int(dim)
         self.nu = nu
         self.rho0 = rho0
         self.lap_vel_prefactor = 2. * (2. + self.dim) * self.nu * self.rho0
@@ -217,8 +358,8 @@ class MomentumEquation(Equation):
         d_aw[d_idx] = 0.
 
     def loop(
-        self, d_idx, s_idx, d_rho, d_cs, d_tau_sfs, d_nu_t, d_p, d_au, d_av, d_aw, s_m,
-        s_rho, s_V, s_p, s_tau_sfs, s_nu_t, VIJ, XIJ, HIJ, R2IJ, RHOIJ1, EPS, WIJ, DWIJ
+        self, d_idx, s_idx, d_rho, d_tau, d_nu_t, d_p, d_au, d_av, d_aw, s_rho, 
+        s_V, s_p, s_tau, s_nu_t, VIJ, XIJ, R2IJ, EPS, DWIJ
     ):
         Vj = s_V[s_idx]
         rhoi = d_rho[d_idx]
@@ -235,7 +376,30 @@ class MomentumEquation(Equation):
 
         # Subfilter Stress Term
         if not self.turb_visc_model == "SMAG_MCG":
-            pass
+            i, j = declare('int', 3)
+            n = 3
+            tau_ij = declare('matrix((3,3))')
+            for i in range(n):
+                for j in range(n):
+                    tau_ij[i*n + j] = d_tau[d_idx*9 + i*n + j] + s_tau[s_idx*9 + i*n + j]
+
+            div_tau = declare('matrix((3,1))')
+
+            if self.dim == 2:
+                div_tau[0] = Vj*(tau_ij[0]*DWIJ[0] + tau_ij[1]*DWIJ[1])
+                div_tau[1] = Vj*(tau_ij[3]*DWIJ[0] + tau_ij[4]*DWIJ[1])
+                div_tau[2] = 0.                
+            elif self.dim == 3:
+                for i in range(n):
+                    for j in range(n):
+                        div_tau[i] = Vj*(tau_ij[i*n + j]*DWIJ[j])                 
+
+            tmp = gradp_fac + lap_vel_fac
+            # Accelerations
+            d_au[d_idx] += tmp * DWIJ[0] + div_tau[0]
+            d_av[d_idx] += tmp * DWIJ[1] + div_tau[1]
+            d_aw[d_idx] += tmp * DWIJ[2] + div_tau[2]
+
         else:
             nu_ti = d_nu_t[d_idx]
             nu_tj = s_nu_t[s_idx]
@@ -244,34 +408,8 @@ class MomentumEquation(Equation):
             div_tau_fac *= vijdotxij * Vj / (R2IJ + EPS)
 
             tmp = gradp_fac + lap_vel_fac + div_tau_fac
-
-
-        # accelerations
-        d_au[d_idx] += tmp * DWIJ[0]
-        d_av[d_idx] += tmp * DWIJ[1]
-        d_aw[d_idx] += tmp * DWIJ[2]
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-### Continuity Equation----------------------------------------------------
-from pysph.sph.wc.basic import (
-    ContinuityEquationDeltaSPHPreStep, ContinuityEquationDeltaSPH
-)
-from pysph.sph.wc.transport_velocity import ContinuityEquation
-
-### Momentum Equation------------------------------------------------------
-from pysph.sph.wc.viscosity import LaminarViscosityDeltaSPH
-
+            # Accelerations
+            d_au[d_idx] += tmp * DWIJ[0]
+            d_av[d_idx] += tmp * DWIJ[1]
+            d_aw[d_idx] += tmp * DWIJ[2]
+            
