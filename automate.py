14dbb98a9c88fe0285a54a5b37e01bde96cf2184
K T Prajwal Prathiksh <prajwal.prathiksh@gmail.com>
2022-11-18 15:51:26 +0530

    Add GenerateSineVelData

--- automate.py
+++ automate.py
@@ -1,26 +1,65 @@
 # Library imports.
 import os
-from itertools import product
 import numpy as np
 import matplotlib.pyplot as plt
 
 # Automan imports
 from automan.api import Automator, Simulation 
 from automan.api import PySPHProblem as Problem
-from automan.automation import filter_cases, filter_by_name
+from automan.api import mdict, dprod, opts2path
 
-class EnergySpectrum2D(Problem):
+BACKEND = " --openmp"
+
+class GenerateSineVelData(Problem):
     def get_name(self):
         """
             Problem name.
         """
-        return 'energy_spectrum_2d'
+        return 'sine_vel_profiles'
 
     def setup(self):
         """
             Setup the problem.
         """
+        base_cmd = "python code/sine_velocity_profile.py " + BACKEND + " "
+        
+        # Create parameteric cases
+        perturb_opts = mdict(perturb=[1e-4, 1e-3])
+        dim_nx_opts = mdict(dim=[1], nx=[31, 51, 101, 151])
+        dim_nx_opts += mdict(dim=[2], nx=[31, 51])
 
-        base_cmd = "python code/energy_spectrum.py"
+        # Add dim_nx_opts to dprod to account for the zero perturbation cases
+        all_options = dprod(perturb_opts, dim_nx_opts) + dim_nx_opts
+
+        # Setup cases
+        self.cases = [
+            Simulation(
+                root=self.input_path(opts2path(kw)),
+                base_command=base_cmd,
+                job_info=dict(n_core=1, n_thread=1),
+                **kw
+            )
+            for kw in all_options
+        ]
+    
+    def run(self):
+        """
+            Run the problem.
+        """
+        self.make_output_dir()
+
+
+
+
+if __name__ == "__main__":
+    PROBLEMS = [
+        GenerateSineVelData
+    ]
+    automator = Automator(
+        simulation_dir='outputs',
+        output_dir=os.path.join('manuscript', 'figures'),
+        all_problems=PROBLEMS
+    )
+    automator.run()
 
     
