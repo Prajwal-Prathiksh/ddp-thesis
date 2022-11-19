57cd44985aa0dde2147d5ce30fa22c57e0ba60ad
K T Prajwal Prathiksh <prajwal.prathiksh@gmail.com>
2022-11-20 02:36:40 +0530

    Add simple analysis

--- automate.py
+++ automate.py
@@ -1,5 +1,6 @@
 # Library imports.
 import os
+import itertools as IT
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -8,9 +9,42 @@ from automan.api import Automator, Simulation
 from automan.api import CondaClusterManager
 from automan.api import PySPHProblem
 from automan.api import mdict, dprod, opts2path
-from automan.utils import filter_by_name, filter_cases, compare_runs
+from automan.utils import filter_cases, compare_runs
 
 BACKEND = " --openmp"
+
+#TODO: Add error checking for SimPlotter
+#TODO: Add uniform_naming for Simplotter methods
+#TODO: Run for multiple nx, perturb, i_nx
+#TODO: Add a GUI for filtering and plotting comparisons plots
+#TODO: Refacto code to make the above possible
+
+def styles(sims):
+    ls = [dict(linestyle=x[0], color=x[1]) for x in
+          IT.product(["-", "--", "-.", ":"], 'kbgrycm')]
+    return IT.cycle(ls)
+
+class SimPlotter(Simulation):
+    def scalar_Ek_loglog(self, **kw):
+        # Load npz file.
+        data = np.load(self.input_path('espec_result_0.npz'))
+        plt.loglog(data['k'], data['Ek'], **kw)
+    
+    def scalar_Ek_loglog_exact(self, **kw):
+        # Load npz file.
+        data = np.load(self.input_path('espec_result_0.npz'))
+        plt.loglog(data['k'], data['Ek_exact'], **kw)
+    
+    def scalar_Ek_plot(self, **kw):
+        # Load npz file.
+        data = np.load(self.input_path('espec_result_0.npz'))
+        # Get label from **kw.
+        plt.plot(data['k'], data['Ek'], **kw)
+    
+    def l2_error(self, **kw):
+        # Load npz file.
+        data = np.load(self.input_path('espec_result_0.npz'))
+        plt.loglog(data['k'], data['l2_error'], **kw)
 
 class SineVelProfile(PySPHProblem):
     def get_name(self):
@@ -24,18 +58,17 @@ class SineVelProfile(PySPHProblem):
             Setup the problem.
         """
         base_cmd = "python code/sine_velocity_profile.py " + BACKEND
-        base_cmd += " --max-steps 0 --no-plots"
+        base_cmd += " --max-steps 0"
         
         # Create parameteric cases
         def get_complete_parametric_opts():
             # Domain parameters
-            perturb_opts = mdict(perturb=[1e-2, 1e-1, 1])
+            perturb_opts = mdict(perturb=[0, 1e-2, 1e-1, 1])
             dim_nx_opts = mdict(dim=[1], nx=[401, 801, 1601])
             dim_nx_opts += mdict(dim=[2], nx=[51, 101, 201])
             dim_nx_opts += mdict(dim=[3], nx=[31, 51, 101])
 
-            # Add dim_nx_opts to dprod to account for zero perturbation cases
-            all_options = dprod(perturb_opts, dim_nx_opts) + dim_nx_opts
+            all_options = dprod(perturb_opts, dim_nx_opts)
 
             # Interpolator parameters
             from code.sine_velocity_profile import (
@@ -49,16 +82,20 @@ class SineVelProfile(PySPHProblem):
             return all_options
 
         def get_example_opts():
-            perturb_opts = mdict(perturb=[1e-1])
-            dim_nx_opts = mdict(dim=[1], nx=[801, 1601])
-            dim_nx_opts += mdict(dim=[2], nx=[51, 101])
+            perturb_opts = mdict(perturb=[0])
+            dim_nx_opts = mdict(dim=[1], nx=[31, 1601])
+            dim_nx_opts += mdict(dim=[2], nx=[21, 101, 201])
+            dim_nx_opts += mdict(dim=[3], nx=[21, 51])
 
-            all_options = dprod(perturb_opts, dim_nx_opts) + dim_nx_opts
+            all_options = dprod(perturb_opts, dim_nx_opts)
 
-            KERNEL_CHOICES = [
-                'WendlandQuinticC2', 'WendlandQuinticC4', 'Gaussian'
-            ]
-            INTERPOLATING_METHOD_CHOICES = ['sph', 'shepard', 'order1']
+            # KERNEL_CHOICES = [
+            #     'WendlandQuinticC4'
+            # ]
+            from code.sine_velocity_profile import KERNEL_CHOICES
+
+            INTERPOLATING_METHOD_CHOICES = ['shepard']
+            
             i_kernel_opts = mdict(i_kernel=KERNEL_CHOICES)
             i_method_opts = mdict(i_method=INTERPOLATING_METHOD_CHOICES)
             interpolator_opts = dprod(i_kernel_opts, i_method_opts)
@@ -66,8 +103,7 @@ class SineVelProfile(PySPHProblem):
             all_options = dprod(all_options, interpolator_opts)
             return all_options
 
-        all_options = get_complete_parametric_opts()
-        all_options = get_example_opts()       
+        all_options = get_example_opts()
 
         # Setup cases
         self.cases = [
@@ -86,6 +122,108 @@ class SineVelProfile(PySPHProblem):
         """
         self.make_output_dir()
 
+        def temp_plot(dim, nx, labels, plot_type, title):
+            plt.figure()
+            filtered_cases = filter_cases(
+                self.cases, dim=dim, nx=nx, perturb=0,
+            )
+            if plot_type == 'loglog':
+                compare_runs(
+                    sims=filtered_cases, method=SimPlotter.scalar_Ek_loglog,
+                    exact=SimPlotter.scalar_Ek_loglog_exact, labels=labels, styles=styles
+                )
+            elif plot_type == 'plot':
+                compare_runs(
+                    sims=filtered_cases, method=SimPlotter.scalar_Ek_plot,
+                    exact=None, labels=labels, styles=styles
+                )
+            elif plot_type == 'l2_error':
+                compare_runs(
+                    sims=filtered_cases, method=SimPlotter.l2_error,
+                    exact=None, labels=labels, styles=styles
+                )
+            plt.title(title)
+            plt.xlabel('k')
+            plt.ylabel('E(k)')
+            plt.legend()
+            plt.savefig(self.output_path(title + '.png'))
+            plt.close()
+        
+        temp_plot(
+            dim=1, nx=1601,
+            labels=['i_kernel'],
+            plot_type='l2_error',
+            title='1D_i_kernel_l2_error'
+        )
+        temp_plot(
+            dim=2, nx=201,
+            labels=['i_kernel'],
+            plot_type='l2_error',
+            title='2D_i_kernel_l2_error'
+        )
+        temp_plot(
+            dim=3, nx=51,
+            labels=['i_kernel'],
+            plot_type='l2_error',
+            title='3D_i_kernel_l2_error'
+        )
+
+        temp_plot(
+            dim=1, nx=1601,
+            labels=['i_method'],
+            plot_type='loglog',
+            title='1D_i_method_loglog'
+        )
+        temp_plot(
+            dim=1, nx=1601,
+            labels=['i_method'],
+            plot_type='l2_error',
+            title='1D_i_method_l2_error'
+        )
+        temp_plot(
+            dim=1, nx=31,
+            labels=['i_method'],
+            plot_type='plot',
+            title='1D_i_method_plot'
+        )
+        temp_plot(
+            dim=2, nx=101,
+            labels=['i_method'],
+            plot_type='loglog',
+            title='2D_i_method_loglog'
+        )
+        temp_plot(
+            dim=2, nx=101,
+            labels=['i_method'],
+            plot_type='l2_error',
+            title='2D_i_method_l2_error'
+        )
+        temp_plot(
+            dim=2, nx=21,
+            labels=['i_method'],
+            plot_type='plot',
+            title='2D_i_method_plot'
+        )
+        temp_plot(
+            dim=3, nx=51,
+            labels=['i_method'],
+            plot_type='loglog',
+            title='3D_i_method_loglog'
+        )
+        temp_plot(
+            dim=3, nx=51,
+            labels=['i_method'],
+            plot_type='l2_error',
+            title='3D_i_method_l2_error'
+        )
+        temp_plot(
+            dim=3, nx=21,
+            labels=['i_method'],
+            plot_type='plot',
+            title='3D_i_method_plot'
+        )
+
+
 
 if __name__ == "__main__":
     PROBLEMS = [
@@ -93,7 +231,7 @@ if __name__ == "__main__":
     ]
     automator = Automator(
         simulation_dir='outputs',
-        output_dir=os.path.join('manuscript', 'figures'),
+        output_dir=os.path.join('manuscript', 'code_figures'),
         all_problems=PROBLEMS,
         cluster_manager_factory=CondaClusterManager
     )
