77b80601437bab962d26f6506fe25b56efb85ef4
K T Prajwal Prathiksh <prajwal.prathiksh@gmail.com>
2022-11-22 22:06:31 +0530

    Define a custom_runs function

--- code/automate_utils.py
+++ code/automate_utils.py
@@ -4,6 +4,7 @@ Utilary functions and classes for the automate.py script.
 """
 # Library imports.
 import os
+import collections
 import itertools as IT
 import numpy as np
 import matplotlib.pyplot as plt
@@ -34,3 +35,36 @@ def styles(sims):
     ls = [dict(linestyle=x[0], color=x[1]) for x in
           IT.product(["-", "--", "-.", ":"], 'kbgrycm')]
     return IT.cycle(ls)
+
+def custom_compare_runs(
+    sims:list, method:callable, labels:list, exact:callable=None,
+    styles=styles, exact_sim_idx:int=0
+):
+    """
+    Given a sequence of Simulation instances, a method name, the labels to
+    compare and an optional method name for an exact solution, this calls the
+    methods with the appropriate parameters for each simulation.
+
+    Parameters
+    ----------
+    sims: sequence
+        Sequence of `Simulation` objects.
+    method: callable
+        The plotter method to call on each simulation.
+    labels: sequence
+        Sequence of parameters to use as labels for the plot.
+    exact: callable
+        The exact solution method to call on the simulation.
+    styles: callable: returns an iterator/iterable of style keyword arguments.
+        Defaults to the ``styles`` function defined in this module.
+    exact_sim_idx: int
+        The index of the simulation to use for the exact solution.
+        Default is 0.
+    """
+    ls = styles(sims)
+    if isinstance(ls, collections.abc.Iterable):
+        ls = iter(ls)
+    if exact is not None:
+        exact(sims[exact_sim_idx], **next(ls))
+    for s in sims:
+        method(s, label=s.get_labels(labels), **next(ls))         
