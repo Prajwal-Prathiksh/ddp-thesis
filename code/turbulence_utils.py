abac89a7be8f8943c0a1a81cafc809a1ee6eef0b
K T Prajwal Prathiksh <prajwal.prathiksh@gmail.com>
2023-02-16 19:20:36 +0530

    Add compute_curl

--- code/turbulence_utils.py
+++ code/turbulence_utils.py
@@ -71,26 +71,70 @@ def get_kernel_cls(name: str, dim: int):
     return mapper[name](dim=dim)
 
 
-def curl_numerical(u, v, w, dx):
+def compute_curl(
+    dx: float, u: np.ndarray, v: np.ndarray, w: np.ndarray = None,
+    edge_order: int = 2
+):
     """
-    Returns the numerical curl of a vector field.
+    Compute the curl of a vector field (2D or 3D), assuming a uniform grid,
+    of spacing `dx`.
+    The function computes the curl of the vector field (u, v, w) using
+    `np.gradient`.
+    
+    Parameters
+    ----------
+    dx : float
+        Spacing between grid points.
+    u : np.ndarray
+        x-component of the vector field.
+    v : np.ndarray
+        y-component of the vector field.
+    w : np.ndarray, optional
+        z-component of the vector field. Default is None.
+    edge_order : int, optional
+        Order of the edge gradient. Default is 2.
+    
+    Returns
+    -------
+    curl_z (2D) or curl_x, curl_y, curl_z (3D) : np.ndarray
+        The curl of the vector field.
     """
     def _dim(u):
+        """
+        Return the dimensionality of the array `u`.
+        """
         return len(np.shape(u))
-    if _dim(u) != _dim(v):
-        raise ValueError('u and v must have the same dimensionality')
+    
+    def _check_shape(*args):
+        """
+        Check if the arrays have the same shape.
+        """
+        shape = np.shape(args[0])
+        for arg in args:
+            if np.shape(arg) != shape:
+                raise ValueError('All arrays must have the same shape')
+    
     if _dim(u) == 2:
-        du_dy, du_dx = np.gradient(u, dx, edge_order=2)
-        dv_dy, dv_dx = np.gradient(v, dx, edge_order=2)
+        _check_shape(u, v)
+        du_dy, du_dx = np.gradient(u, dx, edge_order=edge_order)
+        dv_dy, dv_dx = np.gradient(v, dx, edge_order=edge_order)
+
         curl_z = dv_dx - du_dy
+
         return curl_z
+
     elif _dim(u) == 3:
-        du_dy, du_dx, du_dz = np.gradient(u, dx, edge_order=2)
-        dv_dy, dv_dx, dv_dz = np.gradient(v, dx, edge_order=2)
-        dw_dy, dw_dx, dw_dz = np.gradient(w, dx, edge_order=2)
+        _check_shape(u, v, w)
+        du_dy, du_dx, du_dz = np.gradient(u, dx, edge_order=edge_order)
+        dv_dy, dv_dx, dv_dz = np.gradient(v, dx, edge_order=edge_order)
+        dw_dy, dw_dx, dw_dz = np.gradient(w, dx, edge_order=edge_order)
+
         curl_x = dw_dy - dv_dz
         curl_y = du_dz - dw_dx
         curl_z = dv_dx - du_dy
+        
         return curl_x, curl_y, curl_z
+
     else:
-        raise ValueError('Invalid dimensionality')
+        msg = f"Invalid dimensionality: {_dim(u)}. Must be 2 or 3."
+        raise ValueError(msg)
