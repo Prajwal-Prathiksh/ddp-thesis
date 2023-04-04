e44319644703b86c9f37954a06a9a24ca691b3af
K T Prajwal Prathiksh <prajwal.prathiksh@gmail.com>
2023-04-04 21:12:55 +0530

    Add util funcs

--- code/automate_utils.py
+++ code/automate_utils.py
@@ -5,6 +5,8 @@ Author: K T Prajwal Prathiksh
 """
 # Library imports
 import os
+import glob
+import json
 import collections
 import itertools as IT
 import numpy as np
@@ -18,6 +20,20 @@ from automan.api import PySPHProblem
 from automan.api import mdict, dprod, opts2path
 from automan.utils import filter_cases, compare_runs
 
+LINESTYLE = [
+    (0, (1, 1)),
+    (0, (5, 5)),
+    (0, (3, 5, 1, 5)),
+    (0, (3, 5, 1, 5, 1, 5)),
+    (0, (1, 1)),
+    (0, (5, 1)),
+    (0, (3, 1, 1, 1)),
+    (0, (3, 1, 1, 1, 1, 1))
+]
+
+MARKER = [
+    '*', 'o', '<', '+', '^', 'x', '>', '.'
+]
 
 def styles(sims):
     """
@@ -79,3 +95,86 @@ def plot_vline(k, n=2):
     plt.axvline(k_by_n, color='y', linestyle='--')
     txt = f'k/{n}'
     plt.annotate(r'$' + txt + r'$', xy=(k_by_n*1.1, 1e-2), color='y')
+
+def get_all_unique_values(list_of_dicts, key, option=None):
+    """
+    Get all unique values of a key in a list of dictionaries.
+
+    Parameters
+    ----------
+    list_of_dicts : list of dict
+        The list of dictionaries.
+    key : str
+        The key for which the unique values are to be returned.
+    option : dict, optional
+        The option to filter the list of dictionaries. The default is None.
+
+    Returns
+    -------
+    res : list
+        The list of unique values of the key in the list of dictionaries.
+    
+    Example
+    -------
+    >>> get_all_unique_values([{'a': 1, 'b': 2}, {'a': 2, 'b': 2}], 'a')
+    [1, 2]
+    >>> get_all_unique_values([{'a': 1, 'b': 1}, {'a': 2, 'b': 2}], 'a', {'b': 2})
+    [2]
+    """
+    if option is None:
+        res = list(set([d[key] for d in list_of_dicts]))
+        res.sort()
+    else:
+        temp_list = []
+        for k, v in option.items():
+            valids = [d for d in list_of_dicts if d[k] == v]
+            for d in valids:
+                if d not in temp_list:
+                    temp_list.append(d)
+        res = list(set([d[key] for d in temp_list]))
+        res.sort()
+    return res
+
+def get_cpu_time(case):
+    """
+    Get the CPU time from the info file.
+
+    Parameters
+    ----------
+    case : Case
+        The case for which the CPU time is to be returned.
+    
+    Returns
+    -------
+    cpu_time : float
+        The CPU time of the case.
+    """
+    info = glob.glob(case.input_path('*.info'))[0]
+    with open(info) as fp:
+        data = json.load(fp)
+    return round(data['cpu_time'], 2)
+
+def get_label_from_scheme(scheme):
+    """
+    Get the label for a given scheme.
+
+    Parameters
+    ----------
+    scheme : str
+        The scheme for which the label is to be returned.
+    
+    Returns
+    -------
+    label : str
+        The label for the given scheme.
+    """
+    if "tvf"==scheme:
+        return 'TVF'
+    elif 'ewcsph' ==scheme:
+        return 'EWCSPH'
+    elif 'delta_plus' ==scheme:
+        return r'$\delta^{+}SPH$'
+    elif 'tsph'==scheme:
+        return 'L-IPST-C'
+    elif 'edac' ==scheme:
+        return 'EDAC'
